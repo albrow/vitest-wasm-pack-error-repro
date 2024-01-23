ac2b9f4299ec76b9dc184c2e2340fc11d63ec8fb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-23 13:56:53 +0900

    fix: load wasm-bindgen module on vitest

--- README.md
+++ README.md
@@ -30,5 +30,18 @@ npm run build
 ### Run tests with vitest.
 
 ```sh
-npm test:js
+pnpm test:js
+
+> vitest-wasm-pack-error-repro@0.1.0 test:js /home/hiroshi/code/tmp/vitest-wasm-pack-error-repro
+> vitest run
+
+
+ RUN  v0.26.3 /home/hiroshi/code/tmp/vitest-wasm-pack-error-repro
+
+ ✓ web/test.ts (1)
+
+ Test Files  1 passed (1)
+      Tests  1 passed (1)
+   Start at  13:57:48
+   Duration  1.08s (transform 259ms, setup 0ms, collect 21ms, tests 3ms)
 ```
--- web/test.ts
+++ web/test.ts
@@ -1,7 +1,17 @@
-import { expect, test } from "vitest";
+import { expect, test, beforeAll } from "vitest";
+import fs from "node:fs"
 
-import { add } from "../math-lib/pkg";
+// can reference full path, otherwise it requires `resolve.mainFields: ["module"]`
+import initWasm, { add } from "../math-lib/pkg/math_lib.js";
+
+// wasm-pack's web build is not supposed to run on Vitest/NodeJS.
+// but you can still provide wasm module manually to initialize wasm-bindgen based js module.
+beforeAll(async () => {
+  const wasmSource = await fs.promises.readFile("./math-lib/pkg/math_lib_bg.wasm");
+  const wasmModule = await WebAssembly.compile(wasmSource);
+  await initWasm(wasmModule);
+});
 
 test("wasm add function", () => {
-  expect(add(2, 2)).toBe(2);
+  expect(add(2, 2)).toBe(4);
 });
