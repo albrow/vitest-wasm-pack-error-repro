6e370b2e69945c65df923e90cb0427ea4980e39f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-23 13:34:07 +0900

    chore: cleanup deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import wasmPack from "vite-plugin-wasm-pack";
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    clearScreen: false,
     root: "web",
     plugins: [wasmPack("./math-lib")],
   };
